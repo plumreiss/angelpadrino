4da5f21469681c4198fb70fb59571084efd3c37d
Angel Padrino <plumreiss@gmail.com>
2021-10-06 00:46:07 -0400

    feat: add main content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,21 @@
+import Image from "next/image";
+import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
+
 export default function Main() {
-  return <h1>Starting web page</h1>;
+  return (
+    <div>
+      <div>
+        <Image
+          src="/my-profile-photo.jpg"
+          height={200}
+          width={200}
+          alt="Angel Padrino"
+        />
+        <h1>Hey there! I'm a Fronted Developer</h1>
+        <FaInstagram />
+        <FaLinkedin />
+        <FaGithub />
+      </div>
+    </div>
+  );
 }
